aa48e30af582499c4deb39b7d25eb7239549636e
Varun S Athreya <varun.sathreya@gmail.com>
2020-06-28 20:29:12 +0530

    created signin route

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const { check, validationResult } = require("express-validator");
 
-const { signout, signup } = require("../controllers/auth");
+const { signout, signup, signin } = require("../controllers/auth");
 
 router.post(
     "/signup",
@@ -17,6 +17,18 @@ router.post(
     ],
     signup
 );
+
+router.post(
+    "/signin",
+    [
+        check("email").isEmail().withMessage("email is requried"),
+        check("password")
+            .isLength({ min: 1 })
+            .withMessage("password field is requried"),
+    ],
+    signin
+);
+
 router.get("/signout", signout);
 
 module.exports = router;

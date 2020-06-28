6f0ea2f2bc9f9cca4c46cb5d3c8feeddc21cdf46
Varun S Athreya <varun.sathreya@gmail.com>
2020-06-29 03:33:21 +0530

    added updateUser and getUser controller

--- controllers/user.js
+++ controllers/user.js
@@ -14,6 +14,31 @@ exports.getUserById = (req, res, next, id) => {
 };
 
 exports.getUser = (req, res) => {
-    //TODO: GET BACK HERE FOR PASSWORD
+    req.profile.salt = undefined;
+    req.profile.encry_password = undefined;
+    req.profile.createdAt = undefined;
+    req.profile.updatedAt = undefined;
+
     return res.json(req.profile);
+};
+
+exports.updateUser = (req, res) => {
+    User.findByIdAndUpdate(
+        { _id: req.profile._id },
+        { $set: req.body },
+        { new: true, useFindAndModify: false },
+        (err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "You are not authrized to update this user",
+                });
+            }
+            user.salt = undefined;
+            user.encry_password = undefined;
+            user.createdAt = undefined;
+            user.updatedAt = undefined;
+
+            return res.json(user);
+        }
+    );
 };

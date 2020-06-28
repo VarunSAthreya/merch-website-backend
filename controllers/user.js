eb321944754c9a362f09fe17a3335495659ea8da
Varun S Athreya <varun.sathreya@gmail.com>
2020-06-29 03:55:51 +0530

    added pushOrderInPurchaseList controller

--- controllers/user.js
+++ controllers/user.js
@@ -56,3 +56,34 @@ exports.userPurchaseList = (req, res) => {
             return res.json(order);
         });
 };
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+    let purchases = [];
+    req.body.order.products.forEach((product) => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id,
+        });
+    });
+
+    // STORE THIS IN DB
+
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $push: { purchases: purchases } },
+        { new: true },
+        (err, purchases) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Unable to save purchases list",
+                });
+            }
+            next();
+        }
+    );
+};

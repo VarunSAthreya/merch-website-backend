7a856b0ef38a88e4b84b65756bf676f9a2905f3b
Varun S Athreya <varun.sathreya@gmail.com>
2020-06-28 20:59:07 +0530

    signin controller added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const { check, validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
     const errors = validationResult(req);
@@ -21,6 +23,49 @@ exports.signup = (req, res) => {
             name: user.name,
             email: user.email,
             id: user._id,
+        });
+    });
+};
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg,
+        });
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "USER email does not exists.",
+            });
+        }
+
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and Password do not match",
+            });
+        }
+
+        // CREATE TOKEN
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+        // PUT TOKEN IN A COOKIE
+        res.cookie("token", token, { expire: new Date() + 9999 });
+
+        // SEND RESPONSE TO FRONT END
+        const { _id, name, email, role } = user;
+        return res.json({
+            token,
+            user: {
+                _id,
+                name,
+                email,
+                role,
+            },
         });
     });
 };
